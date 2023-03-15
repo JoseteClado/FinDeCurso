ee652e8d1b91bad20bc446030975a5417d36ca01
joseparedes@estudiante.edib.es <joseparedes@estudiante.edib.es>
2023-03-15 20:45:08 +0100

    Pagina perfil avanzada

--- public/pages/perfil.php
+++ public/pages/perfil.php
@@ -9,17 +9,59 @@
 </head>
 <body body>
     <div class="containerPerfil">
-        <h1 class="tituloPerfil">Datos del perfil de usuario</h1>
+        <h1 class="tituloPerfil">Datos del  usuario</h1>
         <div class="tarjetaDatosActuales">
+            <h2 class="tituloDatosActuales" >Datos Actuales</h2>
             <?php
             session_start();
 
             echo "<p>Nombre: ".$_SESSION['nombre']."</p>";
             echo "<p>Correo: ".$_SESSION['correo']."</p>";
             echo "<p>Apellidos: ".$_SESSION['apellidos']."</p>";
+            echo "<p>Contraseña: ******</p>";
            
             ?>
         </div>
+        <div class="tarjetaDatosActuales tarjetaCambiarDatos">
+        <h2 class="tituloDatosActuales" >Cambiar Datos</h2>
+            <?php
+        
+        /* INPUT NOMBRE */
+        echo "<div>";
+        echo "<label for='cambiarNombre'>Nuevo nombre</label>";
+        echo '<input name="nombreCambiar" value="'.$_SESSION['nombre'].'">';
+        echo "</div>";
+
+        /* INPUT APELLIDOS */
+        echo "<div>";
+        echo "<label for='cambiarNombre'>Nuevo apellido</label>";
+        echo '<input name="nombreCambiar" value="'.$_SESSION['apellidos'].'">';
+        echo "</div>";
+
+
+        /* INPUT CORREO */
+        echo "<div>";
+        echo "<label for='cambiarNombre'>Nuevo Correo</label>";
+        echo '<input name="nombreCambiar" value="'.$_SESSION['nombre'].'">';
+        echo "</div>";
+
+        /* INPUT CONTRASEÑA */
+        echo "<div>";
+        echo "<label for='cambiarNombre'>Cambiar Nombre</label>";
+        echo '<input name="nombreCambiar" value="'.$_SESSION['nombre'].'">';
+        echo "</div>";
+
+
+        echo "<div>";
+        echo "<label for='cambiarNombre'>Cambiar Nombre</label>";
+        echo '<input name="nombreCambiar" value="'.$_SESSION['nombre'].'">';
+        echo "</div>";
+           
+           
+           
+            ?>
+        </div>
+
     </div>
 </body>
 </html>
